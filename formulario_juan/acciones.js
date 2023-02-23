ca423a06982aeb7bdcb4334809dc8a3ff76c1237
SENA <casanchez4253@misena.edu.co>
2023-02-23 11:27:08 -0500

    Actualizacion Rama Juan

--- formulario_juan/acciones.js
+++ formulario_juan/acciones.js
@@ -5,4 +5,20 @@ function final(){
 
 function genero(){
     var progreso = document.getElementById('progreso')
+    progreso.style = "width: 25%"
+}
+
+function nombre(){
+    var progreso = document.getElementById('progreso')
+    progreso.style = "width: 50%"
+}
+
+function elemento(){
+    var progreso = document.getElementById('progreso')
+    progreso.style = "width: 75%"
+}
+
+function rareza(){
+    var progreso = document.getElementById('progreso')
+    progreso.style = "width: 100%"
 }
