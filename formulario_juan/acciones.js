8cd38da25e4eef84bfabc7513a8f185ceb0af577
SENA <casanchez4253@misena.edu.co>
2023-02-23 11:00:25 -0500

    Subiendo Formulario Juan

---
+++ formulario_juan/acciones.js
@@ -0,0 +1,8 @@
+function final(){
+    var gracias = document.getElementById('gracias')
+    gracias.innerHTML = "Gracias por Contestar ❤️"
+}
+
+function genero(){
+    var progreso = document.getElementById('progreso')
+}
